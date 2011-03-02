c824f38efda6d69bef3fcda0542d3aacb3227a39
Evan Morrison <edm92@me.com>
2011-03-02 00:33:50 -0800

    Edited src/std/strings.java via GitHub

--- src/std/strings.java
+++ src/std/strings.java
@@ -3,4 +3,6 @@ package std;
 public class strings {
 
 	public static String title = "ImWithStupid";
+	public static String fs = System.getProperty("file.separator");
+	public static String home = System.getProperty("user.dir");
 }
